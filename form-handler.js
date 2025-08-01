34bd19a125f267f19b6c3e662bac16586d643bdd
Drew King <andrew.phillip.king@gmail.com>
2025-08-01 08:28:36 -0700

    Prevent XSS attacks.

--- form-handler.js
+++ form-handler.js
@@ -1,32 +1,45 @@
 // JavaScript (form-handler.js)
-document.getElementById('myForm').addEventListener('submit', function(event) {
+
+document.getElementById("myForm").addEventListener("submit", function (event) {
   event.preventDefault();
-  document.getElementById('myForm').style.display = 'none';
+  document.getElementById("myForm").style.display = "none";
   event.preventDefault();
 
   const formData = new FormData(this);
 
-  fetch('formhandler.php', {
-    method: 'POST',
-    body: formData
+  function purifyInput(input) {
+    const sanitizedInput = DOMPurify.sanitize(input, {
+      ALLOWED_TAGS: [],
+      ALLOWED_ATTR: [],
+    });
+    return sanitizedInput;
+  }
+
+  fetch("formhandler.php", {
+    method: "POST",
+    body: formData,
   })
-  .then(response => response.text())
-  .then(result => {
-    try {
-      const parsedResult = JSON.parse(result);
-      document.getElementById('status').textContent = `${parsedResult.status}`;
-      document.getElementById('confirmation').textContent = `${parsedResult.confirmation}`;
-      document.getElementById('message').textContent = `${parsedResult.message}`;
-    } catch (e) {
-      document.getElementById('liveRegion').textContent = `Thank you, your message has been sent:
+    .then((response) => response.text())
+    .then((result) => {
+      try {
+        const parsedResult = JSON.parse(result);
+        const message = purifyInput(parsedResult.message);
+        document.getElementById("status").textContent =
+          `${parsedResult.status}`;
+        document.getElementById("confirmation").textContent =
+          `${parsedResult.confirmation}`;
+        document.getElementById("message").textContent = `${message}`;
+      } catch (e) {
+        document.getElementById("liveRegion").textContent =
+          `Thank you, your message has been sent:
 
 ${result}`;
-    }
-    console.log('Success:', result);
-  })
-  .catch(error => {
-    const errorMessage = 'An error occurred. Please try again.';
-    document.getElementById('status').textContent = errorMessage;
-    console.error('Error:', error);
-  });
+      }
+      console.log("Success:", result);
+    })
+    .catch((error) => {
+      const errorMessage = "An error occurred. Please try again.";
+      document.getElementById("status").textContent = errorMessage;
+      console.error("Error:", error);
+    });
 });
